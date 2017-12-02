c0df47ffeb2aff0777ef4c17d98a7213d7203296
qin.hunter <qin.hunter@qq.com>
2017-12-02 20:04:46 +0800

    update

--- demo/v4.0/compile.sh
+++ demo/v4.0/compile.sh
@@ -4,7 +4,7 @@ gcc -o clear.o clear.c -lrt -lpthread -Wall
 
 gcc -o control.o control.c -lrt -lpthread -Wall
 
-gcc -o packet_sending.o packet_sending.c -lrt -lpthread /usr/lib/x86_64-linux-gnu/libpcap.a /usr/local/lib/libndpi.a -Wall -Wno-unused-function
+gcc -o packet_send.o packet_sending.c -lrt -lpthread /usr/lib/x86_64-linux-gnu/libpcap.a /usr/local/lib/libndpi.a -Wall -Wno-unused-function
 
 gcc -o p1_l3.o p1_l3.c -lrt -lpthread /usr/lib/x86_64-linux-gnu/libpcap.a /usr/local/lib/libndpi.a -Wall -Wno-unused-function
 
